2f0af5e2034c4229f7d3f4ece5e559277dbbc01f
Mark Lilback <mark@lilback.com>
2019-04-19 14:04:59 -0400

    listener connects to k8s

--- appserver/JobAppServer/src/main/java/com/protosinc/rspark/InitializerListener.java
+++ appserver/JobAppServer/src/main/java/com/protosinc/rspark/InitializerListener.java
@@ -7,13 +7,16 @@ import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
 
+import io.fabric8.kubernetes.api.model.ConfigMap;
+import io.fabric8.kubernetes.api.model.DoneableConfigMap;
 import io.fabric8.kubernetes.client.DefaultKubernetesClient;
 import io.fabric8.kubernetes.client.KubernetesClient;
+import io.fabric8.kubernetes.client.dsl.Resource;
 
 @WebListener
 public class InitializerListener implements ServletContextListener {
-	private KubernetesClient k8sclient;
-	private String namespaceName;
+	private KubernetesClient k8sclient;		
+	private String namespaceName, jobTemplateName;
 	
 	@Override
 	public void contextInitialized(ServletContextEvent sce) {
@@ -22,8 +25,24 @@ public class InitializerListener implements ServletContextListener {
 		if (namespaceName == null) {
 			namespaceName = "default";
 		}
-		
+		jobTemplateName = System.getenv("DEPLOY_JOB_TEMPLATE");
+		if (jobTemplateName == null) {
+			throw new IllegalArgumentException("k8s job template not specified");
+		}
+		//open the client
 		k8sclient = new DefaultKubernetesClient();
+		
+		//read job resource
+		ConfigMap cfgMap =  
+				 k8sclient.configMaps()
+				 .inNamespace(namespaceName)
+				 .withName(jobTemplateName).get();
+		if (cfgMap == null) {
+			throw new IllegalArgumentException("failed to load job template configmap");
+		}
+		System.out.println("cm kind = " + cfgMap.getKind());
+		
+		//get list of namespaces and print
 		List<String> names = k8sclient.namespaces().list().getItems().stream()
 				.map(s -> s.getMetadata().getName())
 				.collect(Collectors.toList());
@@ -34,8 +53,7 @@ public class InitializerListener implements ServletContextListener {
 
 	@Override
 	public void contextDestroyed(ServletContextEvent sce) {
-		
-
+		k8sclient.close();
 	}
 
 	public String getNamespaceName() {
