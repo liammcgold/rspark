2dce3ca7608287a21abf8cc35051b8e0311cb44c
liammcgold <31221321+liammcgold@users.noreply.github.com>
2019-05-21 21:25:18 -0400

    Update aws_setup_file.sh

--- aws_setup_file.sh
+++ aws_setup_file.sh
@@ -6,6 +6,8 @@
 # yum install git 
 
 #run as sudo instals docker and docker compose then builds cluster in AWS, made for quickly doing this 
+#bask aws_setup_file.sh
+yum install docker
 yum install -y yum-utils device-mapper-persistent-data lvm2
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 yum install docker-ce
@@ -16,6 +18,5 @@ yum install -y python-pip
 pip install docker-compose
 yum upgrade python*
 
-#build it
-./start.sh build
+
 
