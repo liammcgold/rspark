1b253ed9eb33be2d1ce850c39641cf8224fb9d4e
liammcgold <31221321+liammcgold@users.noreply.github.com>
2019-05-21 21:21:35 -0400

    Create aws_setup_file.sh

---
+++ aws_setup_file.sh
@@ -0,0 +1,21 @@
+#install yum
+#
+
+#install git with
+#sudo su
+# yum install git 
+
+#run as sudo instals docker and docker compose then builds cluster in AWS, made for quickly doing this 
+yum install -y yum-utils device-mapper-persistent-data lvm2
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+yum install docker-ce
+systemctl enable docker.service
+systemctl start docker.service
+yum install epel-release
+yum install -y python-pip
+pip install docker-compose
+yum upgrade python*
+
+#build it
+./start.sh build
+
